f942e9a3c4cfefd5cde4b123af37ef42521805dd
THIRMAL RAJ <158165461+thirmal112@users.noreply.github.com>
2025-06-24 22:58:23 +0530

    Add files via upload
    ● Objective:
    Learn and apply foundational HTML, CSS, and JavaScript concepts.
    
    ● Steps:
    Create a Simple Webpage Using HTML :
    
    Objective: Understand basic HTML elements and structure.
    
    Actions: Build a webpage with headings, paragraphs, images, and links.
    
    Style the Webpage Using CSS :
    
    Objective: Learn how to apply styling with CSS.
    
    Actions: Add colors, fonts, spacing, and layout adjustments to the webpage.
    
    Add Basic JavaScript for Interactivity :
    
    Objective: Implement simple interactions using JavaScript.
    
    Actions: Create a button that triggers an alert message when clicked.

---
+++ Task-1/script.js
@@ -0,0 +1,11 @@
+function showAlert() {
+    alert("Task 1 is done!");
+}
+
+function changeBackgroundLightBlue() {
+    document.body.style.backgroundColor = '#add8e6'; // light blue
+}
+
+function changeBackgroundLightGreen() {
+    document.body.style.backgroundColor = '#90ee90'; // light green
+}
